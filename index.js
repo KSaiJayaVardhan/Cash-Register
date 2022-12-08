2516b40a3eda37569ab304d71d5d850535ea1edc
KSaiJayaVardhan <sjvardhank@gmail.com>
2022-12-08 14:24:03 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var billAmount = document.querySelector("#bill-amount");
+var cashGiven = document.querySelector("#cash-given");
+var checkButton = document.querySelector("#check-button");
+
+checkButton.addEventListener("click", function(){
+    console.log("clicked");
+})
