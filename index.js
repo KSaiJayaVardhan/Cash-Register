9b0619e5e75934f6d8112a4f505a8f2b34832749
KSaiJayaVardhan <sjvardhank@gmail.com>
2022-12-09 00:15:06 +0530

    Updated changes

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 var billAmount = document.querySelector("#bill-amount");
 var cashGiven = document.querySelector("#cash-given");
 var checkButton = document.querySelector("#check-button");
+var errorMessage = document.querySelector("#error-message");
+var numberOfNotes = document.querySelectorAll(".no-of-notes");
 
+var availableNotes = [2000, 500, 100, 20 , 10, 5, 1];
+ 
 checkButton.addEventListener("click", function(){
-    console.log(billAmount.value);
-})
+    errorMessage.style.display = "none";
+    if (billAmount.value > 0){
+        if (cashGiven.value >= billAmount.value){
+            var amountToBeReturned = cashGiven.value - billAmount.value;
+            for (i=0; i < availableNotes.length; i++){
+                var noOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+                amountToBeReturned = amountToBeReturned % availableNotes[i];
+                numberOfNotes[i].innerText = noOfNotes;
+            }
+        }
+        else{
+            showMessage("Cash given should atleast be equal to bill amount");
+        }
+    }
+    else{
+        showMessage("Invalid Amount");
+    }
+    
+});
+
+function showMessage(msg) {
+    errorMessage.style.display = "block";
+    errorMessage.innerText= msg;
+}
+
