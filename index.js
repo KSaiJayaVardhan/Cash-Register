82f970d8d4dda66e4431b672cc5d6baa76c94b13
KSaiJayaVardhan <sjvardhank@gmail.com>
2022-12-09 20:37:28 +0530

    Updated changes

--- index.js
+++ index.js
@@ -1,31 +1,46 @@
 var billAmount = document.querySelector("#bill-amount");
+var nextButton = document.querySelector("#next-button");
+var labelCashGiven = document.querySelector(".label-cashgiven");
 var cashGiven = document.querySelector("#cash-given");
 var checkButton = document.querySelector("#check-button");
 var errorMessage = document.querySelector("#error-message");
+var showReturnChange = document.querySelector(".show-table");
 var numberOfNotes = document.querySelectorAll(".no-of-notes");
 
+
 var availableNotes = [2000, 500, 100, 20 , 10, 5, 1];
- 
-checkButton.addEventListener("click", function(){
-    errorMessage.style.display = "none";
-    if (billAmount.value > 0){
-        if (cashGiven.value >= billAmount.value){
-            var amountToBeReturned = cashGiven.value - billAmount.value;
-            for (i=0; i < availableNotes.length; i++){
-                var noOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
-                amountToBeReturned = amountToBeReturned % availableNotes[i];
-                numberOfNotes[i].innerText = noOfNotes;
-            }
-        }
-        else{
-            showMessage("Cash given should atleast be equal to bill amount");
-        }
-    }
-    else{
-        showMessage("Invalid Amount");
-    }
-    
+
+nextButton.addEventListener("click", function showSecondInput() {
+    labelCashGiven.style.display = "block";
+    cashGiven.style.display = "block";
+    checkButton.style.display = "block"; 
+    nextButton.style.display= "none";
 });
+
+checkButton.addEventListener("click", function validateTheBillAmountAndCashAmount() {
+    errorMessage.style.display = "none";
+    showReturnChange.style.display = "block";
+    if (billAmount.value > 0) {
+        if (cashGiven.value >= billAmount.value) {
+            var amountToBeReturned = cashGiven.value - billAmount.value;
+            changeCalculation(amountToBeReturned);    
+        }
+        else {
+            showMessage("The cash provided must be atleast equal to the bill amount");
+        }
+    }
+    else {
+        showMessage("Please provide an amount greater than 0");
+    }
+});
+
+function changeCalculation(amountToBeReturned) {
+    for (i=0; i < availableNotes.length; i++) {
+        var noOfNotesOfCurrentDenomination = Math.trunc(amountToBeReturned / availableNotes[i]);
+        amountToBeReturned = amountToBeReturned % availableNotes[i];
+        numberOfNotes[i].innerText = noOfNotesOfCurrentDenomination;
+    }
+};
 
 function showMessage(msg) {
     errorMessage.style.display = "block";
